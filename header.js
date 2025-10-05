9ab9a3f699244182038c9629a22df60b9027b90c
bedo2991 <fra.bdn@gmail.com>
2025-10-05 11:07:35 +0200

    Automated build pipeline

--- header.js
+++ header.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name        WME Speed Limits Shortcuts
 // @namespace   wme-sdk-scripts
-// @version     1.1.0
+// @version     1.2.0
 // @description  Sets shortcuts for quickly updating the speed limits. Use the numbers + the ALT key for limits between 0 and 90 and ALT+SHIFT for limits between 100 and 150. This script only supports km/h!
-// @updateURL	https://code.waze.tools/repository/70101218-806f-4c8a-9323-49df8cd97534.user.js
-// @downloadURL https://code.waze.tools/repository/70101218-806f-4c8a-9323-49df8cd97534.user.js
+// @updateURL	https://github.com/bedo2991/sls/releases/latest/download/update.user.js
+// @downloadURL https://github.com/bedo2991/sls/releases/latest/download/release.user.js
 // @author      bedo2991 @ Waze
 // @match       https://www.waze.com/editor*
 // @match       https://beta.waze.com/editor*
